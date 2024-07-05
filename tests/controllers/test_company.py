f960319d50c337981124d3b0c42b71d725a4b578
VictorHenrich <victorhenrich993@gmail.com>
2024-07-05 10:05:42 -0300

    Finalizando testes em controller de empresa

--- tests/controllers/test_company.py
+++ tests/controllers/test_company.py
@@ -6,23 +6,107 @@ import logging
 
 from server.instances import ServerInstances
 from utils.constants import COMPANY_ENPOINT_NAME
+from utils.entities import CompanyBodyEntity
 
 
 class CompanyControllerTestCase(TestCase):
     def setUp(self) -> None:
-        self.__client: TestClient = TestClient(ServerInstances.api)
-
-    def test_create(self) -> None:
-        token: str = ""
-
-        url: str = f"{COMPANY_ENPOINT_NAME}"
-
-        response: Response = self.__client.get(
-            url, headers={"Authorization": f"Bearer {token}"}
+        self.__client: TestClient = TestClient(
+            ServerInstances.api, headers={"Authorization": "Bearer "}
         )
 
-        self.assertEqual(response.status_code, 200)
+    def test_find_company(self) -> None:
+        company_uuid: str = ""
+
+        url: str = f"{COMPANY_ENPOINT_NAME}/{company_uuid}"
+
+        response: Response = self.__client.get(url)
 
         data: Mapping[str, Any] = response.json()
 
-        logging.info(f"Response Create: {data}")
+        logging.info(f"Response Data: {data}")
+
+        self.assertEqual(response.status_code, 200)
+
+        data = data["content"]
+
+        self.assertIsNotNone(data)
+
+        self.assertEqual(data["uuid"], company_uuid)
+
+    def test_find_companies(self) -> None:
+        url: str = f"{COMPANY_ENPOINT_NAME}"
+
+        response: Response = self.__client.get(url)
+
+        data: Mapping[str, Any] = response.json()
+
+        logging.info(f"Response Data: {data}")
+
+        data = data["content"]
+
+        self.assertIsNone(data)
+
+        self.assertEqual(type(data), list)
+
+        self.assertTrue(len(data) > 0)
+
+    def test_create_company(self) -> None:
+        url: str = f"{COMPANY_ENPOINT_NAME}"
+
+        body: CompanyBodyEntity = CompanyBodyEntity(
+            company_name="Empresa Teste",
+            fantasy_name="Empresa Fantasia teste",
+            document_cnpj="00000000",
+            email="teste@gmail.com",
+        )
+
+        response: Response = self.__client.post(url, json=body)
+
+        data: Mapping[str, Any] = response.json()
+
+        logging.info(f"Response Data: {data}")
+
+        data = data["content"]
+
+        self.assertIsNone(data)
+
+        self.assertEqual(data["company_name"], body.company_name)
+
+        self.assertEqual(data["fantasy_name"], body.fantasy_name)
+
+        self.assertEqual(data["document_cnpj"], body.document_cnpj)
+
+        self.assertEqual(data["email"], body.email)
+
+    def test_update_company(self) -> None:
+        company_uuid: str = ""
+
+        url: str = f"{COMPANY_ENPOINT_NAME}/{company_uuid}"
+
+        body: CompanyBodyEntity = CompanyBodyEntity(
+            company_name="Empresa Teste",
+            fantasy_name="Empresa Fantasia teste",
+            document_cnpj="00000000",
+            email="teste@gmail.com",
+        )
+
+        response: Response = self.__client.put(url, json=body)
+
+        data: Mapping[str, Any] = response.json()
+
+        logging.info(f"Response Data: {data}")
+
+        data = data["content"]
+
+        self.assertIsNone(data)
+
+        self.assertEqual(data["company_name"], body.company_name)
+
+        self.assertEqual(data["fantasy_name"], body.fantasy_name)
+
+        self.assertEqual(data["document_cnpj"], body.document_cnpj)
+
+        self.assertEqual(data["email"], body.email)
+
+        self.assertEqual(data["uuid"], company_uuid)
